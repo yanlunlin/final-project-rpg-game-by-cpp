a89597c84db0175cfbf7024e6a85a64e5651e035
brian0951027-ctrl <brian0951027@gmail.com>
2026-06-14 03:05:17 +0800

    add necessary player variable, constructor, and function

--- player.h
+++ player.h
@@ -1,8 +1,32 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-class Player{
+#include<string>
+//#include"item.h"
 
+using std::string;
+
+class Player{
+    private:
+        string name;
+        int hp;
+        int attackPower;
+        //Item item;
+    public:
+        Player();
+
+        string getName();
+        int getHp();
+        int getAttackPower();
+
+        void setName(string);
+        void setHp(int);
+        void setAttackPower(int);
+
+        void attack();
+        void takeDamage();
+        void useItem();
+        bool isAlive();
 };
 
 class Swordman: public Player{
