06e73e77dfe090440c6074bd400b37973d1b087d
BrianMao <Maochihsheng@gmail.com>
2026-06-14 16:50:11 +0800

    update Item.h with code

---
+++ Item.h
@@ -0,0 +1,26 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <string>
+
+class Item {
+private:
+    std::string name;
+    std::string type;
+    int effectValue;
+    int quantity;
+
+public:
+    Item(std::string itemName, std::string itemType, int value, int qty);
+
+    std::string getName();
+    std::string getType();
+    int getEffectValue();
+    int getQuantity();
+    void setQuantity(int qty);
+    bool isAvailable();
+    bool use();
+    void showInfo();
+};
+
+#endif
