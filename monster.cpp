3aeb323e507b56c6727209ad617976a5e8156076
yanlunlin <yenlunlin1008@gmail.com>
2026-06-16 20:33:22 +0800

    update moster.cpp

--- monster.cpp
+++ monster.cpp
@@ -1,37 +1,13 @@
 #include "monster.h"
+#include "creature.h"
 #include "player.h"
 #include <iostream>
 
-Monster::Monster() : name(""), rewardGold(0) {}
+Monster::Monster() : Creature(), rewardGold(0) {}
 Monster::Monster(string name, int hp, int attackPower, int rewardGold)
-    : name(name), rewardGold(rewardGold) {
-  proper["hp"] = hp;
-  proper["atk"] = attackPower;
-}
+    : Creature(name, hp, attackPower), rewardGold(rewardGold) {}
 
-string Monster::getName() const { return name; }
-unsigned int Monster::getHp() const { return proper.at("hp"); }
-unsigned int Monster::getAttackPower() const { return proper.at("atk"); }
 int Monster::getRewardGold() const { return rewardGold; }
-
-void Monster::attack(Player &target) const {
-  target.takeDamage(*this, getAttackPower());
-}
-bool Monster::takeDamage(Player &target, int damage) {
-  if (damage >= getHp()) {
-    target.setHp(0);
-    return 0;
-  } else {
-    target.setHp(getHp() - damage);
-    return 1;
-  }
-  return 1;
-}
-
-bool Monster::isAlive() { return getHp(); }
-void Monster::showInfo() {
-  cout << getName() << endl << "|-hp: " << getHp() << endl;
-}
 
 Slime::Slime() : Monster("Slime", 0, 0, 0) {}
 Slime::Slime(string name, int hp, int attackPower, int rewardGold)
