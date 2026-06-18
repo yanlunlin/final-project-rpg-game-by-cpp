e7166e7b302b7dd7032df042d35817aa6774e892
brian0951027-ctrl <brian0951027@gmail.com>
2026-06-18 15:15:46 +0800

    add different jobs

--- player.cpp
+++ player.cpp
@@ -8,18 +8,6 @@ using std::vector;
 Player::Player(){}
 Player::Player(string theName, int theHp, int theAttackPower){}
 
-string Player::getName() const{
-    return name;
-}
-
-int Player::getHp() const{
-    return hp;
-}
-
-int Player::getAttackPower() const{
-    return attackPower;
-}
-
 const Inventory& Player::getBackpack() const{
     return backpack;
 }
@@ -28,30 +16,14 @@ const vector<Item>& Player::getItemList() const{
     return backpack.getStorage();
 }
 
-void Player::setName(string theName){
-    name = theName;
-}
-
-void Player::setHp(int theHp){
-    hp = (theHp < 0) ? 0 : theHp;
-}
-
-void Player::setAttackPower(int theAttackPower){
-    attackPower = (theAttackPower < 0) ? 0 : theAttackPower;
-}
-
-void Player::attack(Monster& target){
-    target.takeDamage(*this, getAttackPower());
+/*void Player::attack(Monster& target){
+    target.takeDamage(getAttackPower());
 }
 
 void Player::takeDamage(const Monster& attacker, int damage){
     setHp(hp - damage);
-}
+}*/
 
 void Player::useItem(size_t index){
     backpack.useItem(index);
-}
-
-bool Player::isAlive(){
-    return (hp > 0) ? false : true;
 }
--- player.h
+++ player.h
@@ -4,6 +4,7 @@
 #include<string>
 #include<map>
 #include<vector>
+#include"creature.h"
 #include"inventory.h"
 #include"item.h"
 
@@ -13,40 +14,98 @@ using std::vector;
 
 class Monster;
 
-class Player{
+class Player: public Creature{
     private:
-        string name;
         /*map<string, unsigned int> status{
             {"hp", 0}, {"mp", 0}, {"str", 0}, {"vit", 0}, {"agi", 0}, {"dex", 0}, {"int", 0}
         };*/
-        int hp;
-        int attackPower;
+
+        /*map<string, unsigned int> status{
+            {"hp", 0}, {"mp", 0},
+            {"agi", 0}, //agility 敏捷
+            {"atk", 0}, {"matk", 0},
+            {"def", 0}, {"mdef", 0},
+            {"dex", 0}, //dexterity 影響暴擊率及命中率, 影響命中率較多
+            {"luk", 0}, //luck 影響暴擊率及命中率, 影響暴擊率較多
+        }*/
+
         Inventory backpack;
     public:
         Player();
         Player(string theName, int theHp, int theAttackPower);
 
-        string getName() const;
-        int getHp() const;
-        int getAttackPower() const;
         const Inventory& getBackpack() const;
         const vector<Item>& getItemList() const;
 
-        void setName(string theName);
-        void setHp(int theHp);
-        void setAttackPower(int theAttackPower);
-
-        void attack(Monster& target);
-        void takeDamage(const Monster& attacker, int damage);
+        /*void attack(Monster& target);
+        void takeDamage(const Monster& attacker, int damage);*/
         void useItem(size_t index);
-
-        bool isAlive();
 };
 
 class Swordman: public Player{
     public:
         Swordman();
         Swordman(string theName, int theHp, int theAttackPower);
+};
+
+class SwordMaster: public Swordman{
+    public:
+        SwordMaster();
+        SwordMaster(string theName, int theHp, int theAttackPower);
+};
+
+class Warrior: public Player{
+    public:
+        Warrior();
+        Warrior(string theName, int theHp, int theAttackPower);
+};
+
+class Crusader: public Warrior{
+    public:
+        Crusader();
+        Crusader(string theName, int theHp, int theAttackPower);
+};
+
+class Wizard: public Player{
+    public:
+        Wizard();
+        Wizard(string theName, int theHp, int theAttackPower);
+};
+
+class ArchWizard: public Wizard{
+    public:
+        ArchWizard();
+        ArchWizard(string theName, int theHp, int theAttackPower);
+};
+
+class Priest: public Player{
+    public:
+        Priest();
+        Priest(string theName, int theHp, int theAttackPower);
+};
+
+class ArchPriest: public Priest{
+    public:
+        ArchPriest();
+        ArchPriest(string theName, int theHp, int theAttackPower);
+};
+
+class Archer: public Player{
+    public:
+        Archer();
+        Archer(string theName, int theHp, int theAttackPower);
+};
+
+class Thief: public Player{
+    public:
+        Thief();
+        Thief(string theName, int theHp, int theAttackPower);
+};
+
+class Assassin: public Thief{
+    public:
+        Assassin();
+        Assassin(string theName, int theHp, int theAttackPower);
 };
 
 #endif
