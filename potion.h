3cc289a7864c478d3b781f12104b7c032bca9b0d
BrianMao <Maochihsheng@gmail.com>
2026-06-19 20:10:29 +0800

    Add code to potion classes

---
+++ potion.h
@@ -0,0 +1,13 @@
+#ifndef POTION_H
+#define POTION_H
+
+#include "item.h"
+#include <string>
+
+class Potion : public item {
+public:
+    // statKey -> "hp", "mp", "agi", "atk"
+    Potion(std::string potionName, std::string statKey, int effectValue, int amount);
+};
+
+#endif
