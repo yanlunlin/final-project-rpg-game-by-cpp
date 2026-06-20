15a5e7a4dbdbb63ecde2b684684d7c302352fa91
brian0951027-ctrl <brian0951027@gmail.com>
2026-06-20 14:47:54 +0800

    change status

--- creature.h
+++ creature.h
@@ -3,16 +3,33 @@
 #include <string>
 using namespace std;
 
+struct Stat{
+  unsigned int base;//基礎值
+  int bonusFlat;    //固定值加成
+  int bonusPercent; //百分比加成
+
+  unsigned int getFinalValue() const{
+    int rawFinal = static_cast<int>(base) + bonusFlat;
+    if (rawFinal < 0){
+      rawFinal = 0;
+    }
+
+    double multiplier = 1.0 + (bonusPercent / 100.0);
+    return static_cast<unsigned int>(rawFinal * multiplier);
+  }
+};
+
 class Creature {
 protected:
   string name;
-  map<string, unsigned int> status = {
-    {"hp", 0}, {"mp", 0},
-    {"agi", 0}, //agility 敏捷
-    {"atk", 0}, {"matk", 0},
-    {"def", 0}, {"mdef", 0},
-    {"dex", 0}, //dexterity 影響暴擊率及命中率, 影響命中率較多
-    {"luk", 0}, //luck 影響暴擊率及命中率, 影響暴擊率較多
+  map<string, Stat> status = {
+    {"maxHp", {0, 0, 0}}, {"hp", {0, 0, 0}},
+    {"maxMp", {0, 0, 0}}, {"mp", {0, 0, 0}},
+    {"agi", {0, 0, 0}}, //agility 敏捷
+    {"atk", {0, 0, 0}}, {"matk", {0, 0, 0}},
+    {"def", {0, 0, 0}}, {"mdef", {0, 0, 0}},
+    {"dex", {0, 0, 0}}, //dexterity 影響暴擊率及命中率, 影響命中率較多
+    {"luk", {0, 0, 0}}, //luck 影響暴擊率及命中率, 影響暴擊率較多
   };
 
 public:
@@ -21,7 +38,9 @@ public:
     unsigned int atk, unsigned int matk, unsigned int def, unsigned int mdef,
     unsigned int dex, unsigned int luk);
   string getName() const;
+  unsigned int getMaxHp() const;
   unsigned int getHp() const;
+  unsigned int getMaxMp() const;
   unsigned int getMp() const;
   unsigned int getAgi() const;
   unsigned int getAtk() const;
@@ -31,7 +50,9 @@ public:
   unsigned int getDex() const;
   unsigned int getLuk() const;
   void setName(string name);
+  void setMaxHp(unsigned int maxHp);
   void setHp(unsigned int hp);
+  void setMaxMp(unsigned int maxMp);
   void setMp(unsigned int mp);
   void setAgi(unsigned int agi);
   void setAtk(unsigned int atk);
@@ -44,6 +65,7 @@ public:
   unsigned int getAcc(const Creature& target) const; //Accuracy 命中率
   unsigned int getCri() const; //Critical 暴擊率
 
+  virtual void heal(unsigned int theHp);
   virtual void attack(Creature &target) const;
   virtual void takeDamage(int damage);
   bool isAlive();
